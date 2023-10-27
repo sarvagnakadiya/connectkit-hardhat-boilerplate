d30dde57c5c602bfd6040be74c8f2d6e0dfc13d2
Sarvagna Kadiya <85187416+sarvagnakadiya@users.noreply.github.com>
2023-10-27 13:36:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import { WagmiConfig, createConfig } from "wagmi";
-import {
-  ConnectKitProvider,
-  ConnectKitButton,
-  getDefaultConfig,
-} from "connectkit";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import Page1 from "./Page1";
+import { polygonMumbai } from "wagmi/chains";
+
+const alchemyId = process.env.ALCHEMY_ID;
+const walletConnectProjectId = process.env.WALLETCONNECT_PROJECT_ID;
+
+// Choose which chains you'd like to show
+const chains = [polygonMumbai];
 
 const config = createConfig(
   getDefaultConfig({
-    // Required API Keys
-    alchemyId: process.env.ALCHEMY_ID, // or infuraId
-    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID,
-
-    // Required
     appName: "Your App Name",
-
-    // Optional
-    appDescription: "Your App Description",
-    appUrl: "https://family.co", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+    alchemyId,
+    walletConnectProjectId,
+    chains,
   })
 );
 
@@ -25,7 +22,8 @@ const App = () => {
   return (
     <WagmiConfig config={config}>
       <ConnectKitProvider theme="retro">
-        <ConnectKitButton />
+        <Page1></Page1>
+        {/* <ConnectKitButton /> */}
       </ConnectKitProvider>
     </WagmiConfig>
   );
